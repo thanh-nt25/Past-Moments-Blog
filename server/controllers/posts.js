ca3353033a60a43f7dd39bb238b8f0065c0aeb8e
thanhnguyenTMNp <thanhnguyenqwe65@gmail.com>
2023-10-22 01:59:02 +0700

    bug with update feature

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -24,4 +24,18 @@ export const createPost = async (req, res) => {
     }
 }
 
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const post = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+
+    // await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+}
+
+
 // export default getPosts;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 // routes database
 // tệp cấu hình định tuyến (routing)
 import express from 'express';
-import { getPosts, createPost } from '../controllers/posts.js';
+import { getPosts, createPost,updatePost } from '../controllers/posts.js';
 // import getPosts from '../controllers/posts.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ const router = express.Router();
 // dinh tuyen dia chi goc + '/', nhu localhost:5000/
 router.get('/', getPosts);
 router.post('/', createPost);
+// update
+router.patch('/:id', updatePost);
 
 export default router;
